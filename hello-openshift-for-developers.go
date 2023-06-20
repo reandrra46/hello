16fb9dc1d78e8f57db9814988a6f3fa4bd895df2
reandrra46 <61328210+reandrra46@users.noreply.github.com>
2023-06-20 14:47:16 +0700

    Update hello-openshift-for-developers.go-bitsa

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Rusdi Terapis Promo!"
+		response = "Masak bitsa..Nyuci Bitsa...semua bitsaa!"
 	}
 
 	fmt.Fprintln(w, response)
